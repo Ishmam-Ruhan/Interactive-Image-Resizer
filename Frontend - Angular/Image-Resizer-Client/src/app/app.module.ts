b4affee41e1b7f89c1487fda76e13f99eb71cf1e
Ishmam Ruhan <ishmamruhan@gmail.com>
2022-05-01 12:42:29 +0600

    Angular Toolbar Modified

--- Frontend - Angular/Image-Resizer-Client/src/app/app.module.ts
+++ Frontend - Angular/Image-Resizer-Client/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DragDropDirective } from './Utilities/drag-drop.directive';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DragDropDirective
   ],
   imports: [
     BrowserModule,
